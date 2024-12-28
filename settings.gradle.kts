450c153bf8aec8899ee25f934c28ee081a252038
Vahid Garousi <garousiv@gmail.com>
2024-12-28 11:54:12 +0330

    [#240] Check Java version compatibility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -36,3 +36,11 @@ include(":app-watch:app")
 include(":app-watch:navigation")
 include(":app-watch:ui")
 include(":data:sqldelight")
+
+check(JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
+    """
+    Compose News project requires JDK 17+ but it is currently using JDK ${JavaVersion.current()}.
+    Java Home: [${System.getProperty("java.home")}]
+    https://developer.android.com/build/jdks#jdk-config-in-studio
+    """.trimIndent()
+}
