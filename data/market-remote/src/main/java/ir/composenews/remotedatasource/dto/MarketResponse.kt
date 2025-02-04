2727730eddecfcf96d4cb006e10907b0e05c837f
Kaaveh Mohamedi <Kaaveh@pm.me>
2025-02-04 16:02:15 +0330

    [refactor] Remove unused fields from MarketResponse and TestNewsDtoProvider #250

--- data/market-remote/src/main/java/ir/composenews/remotedatasource/dto/MarketResponse.kt
+++ data/market-remote/src/main/java/ir/composenews/remotedatasource/dto/MarketResponse.kt
@@ -9,8 +9,6 @@ data class MarketResponse(
     val name: String,
     val symbol: String,
     @SerialName("current_price") val currentPrice: Double,
-    @SerialName("market_cap") val marketCap: Double,
-    @SerialName("total_volume") val totalVolume: Double,
     @SerialName("price_change_percentage_24h") val priceChangePercentage24h: Double,
     @SerialName("image") val imageUrl: String,
 )
