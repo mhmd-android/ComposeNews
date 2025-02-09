c208c2791fbfd4bf281b9ec26c12c4b5e4dbe7e1
Vahid Garousi <garousiv@gmail.com>
2025-02-09 14:13:18 +0330

    [#256] Refactor: Migrate Danger to Kotlin and update GitHub Actions
    -   Migrated Danger checks from Ruby to Kotlin.
    -   Introduced `Dangerfile.df.kts` for Kotlin-based Danger configuration.
    -   Removed legacy `Dangerfile` and `Gemfile`.
    -   Updated `danger_checks.yml` GitHub workflow to use the Danger Kotlin action.
    -   Added `.java-version` file to specify the Java version.
    -   Set concurrency in `danger_checks.yml` to cancel in-progress workflows for the same PR.
    -   Upgraded `gradle/gradle-build-action` to `v3`.

---
+++ Dangerfile.df.kts
@@ -0,0 +1,30 @@
+@file:Suppress("MagicNumber", "WildcardImport", "ForbiddenComment")
+
+// Editing this file: https://github.com/danger/kotlin?tab=readme-ov-file#autocomplete-and-syntax-highlighting-in-intellij-idea-or-android-studio
+import systems.danger.kotlin.*
+
+danger(args) {
+
+    onGitHub {
+        val additions = pullRequest.additions ?: 0
+        val deletions = pullRequest.deletions ?: 0
+
+        message("Thanks @${pullRequest.user.login}!")
+
+        if (pullRequest.body.isNullOrBlank()) {
+            fail("Please provide a summary in the Pull Request description.")
+        }
+
+        if (additions > 500) {
+            warn("Please consider breaking up this pull request.")
+        }
+
+        if (issue.labels.isEmpty()) {
+            warn("Please add labels to this PR.")
+        }
+
+        if (deletions > additions) {
+            message("🎉 Code Cleanup!")
+        }
+    }
+}
