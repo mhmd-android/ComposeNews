53f5976835f482b1759479744da6b697e2c2687d
Kaaveh Mohamedi <Kaaveh@pm.me>
2025-02-10 09:13:35 +0330

    [test] Update MarketDaoTest and MarketRepositoryImplTest for comprehensive testing #258

--- data/market-local/src/test/java/ir/composenews/localdatasource/database/MarketDaoTest.kt
+++ data/market-local/src/test/java/ir/composenews/localdatasource/database/MarketDaoTest.kt
@@ -1,11 +1,11 @@
 package ir.composenews.localdatasource.database
 
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
-import io.kotest.matchers.collections.shouldContain
-import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
 import ir.composenews.core_test.SuspendSpec
 import ir.composenews.db.MarketDatabase
-import ir.composenews.localdatasource.test.favoriteMarketEntity
+import ir.composenews.db.MarketEntity
 import kotlinx.coroutines.flow.first
 
 class MarketDaoTest : SuspendSpec({
@@ -18,14 +18,31 @@ class MarketDaoTest : SuspendSpec({
         marketDao = MarketDaoImpl(db)
     }
 
-    "Empty table at db initialization" {
-        val marketList = marketDao.getMarketList().first()
-        marketList.size shouldBeEqual 0
+    "Given database that insert market data, When GetMarketList is called, Then returns list of market entities" {
+        val marketEntities = MarketEntity("1", "Bitcoin", "BTC", 50000.0, 5.0, "url", FALSE)
+
+        marketDao.insertMarket(marketEntities)
+
+        marketDao.getMarketList().first().shouldContainExactly(marketEntities)
     }
 
-    "Insert market to db" {
-        marketDao.insertMarket(favoriteMarketEntity)
-        val marketList = marketDao.getMarketList().first()
-        marketList shouldContain favoriteMarketEntity
+    "Given database that insert favorite market data, When GetFavoriteMarketList is called, Then returns list of favorite market entities" {
+        val favoriteMarkets = MarketEntity("1", "Bitcoin", "BTC", 50000.0, 5.0, "url", TRUE)
+
+        marketDao.insertMarket(favoriteMarkets)
+
+        marketDao.getFavoriteMarketList().first().shouldContainExactly(favoriteMarkets)
+    }
+
+    "Given a list of market entities, When updating the value of Market, Then inserts or updates entities in the database" {
+        val marketEntity = MarketEntity("1", "Bitcoin", "BTC", 50000.0, 5.0, "url", FALSE)
+        marketDao.insertMarket(marketEntity = marketEntity)
+
+        val updatedMarket =
+            marketEntity.copy(currentPrice = 50001.0, priceChangePercentage24h = 5.000001)
+        marketDao.insertMarket(marketEntity = updatedMarket)
+
+        marketDao.getMarketList().first().size shouldBe 1
+        marketDao.getMarketList().first().shouldContainExactly(updatedMarket)
     }
 })
