4d8e3a3bd4b417529392152cfbd7b751e979fc25
Kaaveh Mohamedi <Kaaveh@pm.me>
2025-03-06 20:19:09 +0330

    [refactor] MarketListViewModelTest and MarketDetailViewModelTest #262

--- feature/marketdetail/src/test/java/ir/composenews/marketdetail/MarketDetailViewModelTest.kt
+++ feature/marketdetail/src/test/java/ir/composenews/marketdetail/MarketDetailViewModelTest.kt
@@ -1,30 +1,33 @@
 package ir.composenews.marketdetail
 
+import app.cash.turbine.test
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
+import ir.composenews.base.LoadableData
 import ir.composenews.core_test.MainCoroutineListener
 import ir.composenews.core_test.dispatcher.TestDispatcherProvider
+import ir.composenews.domain.model.Market
 import ir.composenews.domain.model.MarketChart
+import ir.composenews.domain.model.MarketDetail
 import ir.composenews.domain.use_case.GetMarketChartUseCase
 import ir.composenews.domain.use_case.GetMarketDetailUseCase
 import ir.composenews.domain.use_case.ToggleFavoriteMarketListUseCase
 import ir.composenews.network.Errors
 import ir.composenews.network.Resource
 import ir.composenews.uimarket.mapper.toMarket
+import ir.composenews.uimarket.mapper.toMarketModel
 import ir.composenews.uimarket.model.MarketModel
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestCoroutineScheduler
-import kotlinx.coroutines.test.advanceUntilIdle
-import kotlinx.coroutines.test.runTest
-import java.net.SocketTimeoutException
 import java.util.UUID
-import kotlin.random.Random
 
 class MarketDetailViewModelTest : StringSpec({
-
     val getMarketChartUseCase: GetMarketChartUseCase = mockk(relaxed = true)
     val getMarketDetailUseCase: GetMarketDetailUseCase = mockk(relaxed = true)
     val toggleFavoriteMarketListUseCase: ToggleFavoriteMarketListUseCase = mockk(relaxed = true)
@@ -34,112 +37,125 @@ class MarketDetailViewModelTest : StringSpec({
 
     listeners(MainCoroutineListener())
 
-    beforeSpec {
+    beforeEach {
         viewModel = MarketDetailViewModel(
-            getMarketChartUseCase = getMarketChartUseCase,
-            getMarketDetailUseCase = getMarketDetailUseCase,
-            toggleFavoriteMarketListUseCase = toggleFavoriteMarketListUseCase,
-            dispatcherProvider = dispatcherProvider,
+            getMarketChartUseCase,
+            getMarketDetailUseCase,
+            toggleFavoriteMarketListUseCase,
+            dispatcherProvider,
         )
     }
 
-    "With SetMarket event should update state with received market" {
-        runTest {
-            val market = provideFakeMarket()
+    "Given initial state, When ViewModel is created, Then state should be initial" {
+        val initialState = viewModel.state.value
 
-            viewModel.event(MarketDetailContract.Event.SetMarket(market))
-            advanceUntilIdle()
+        initialState.market.shouldBeInstanceOf<LoadableData.Initial>()
+        initialState.marketDetail.shouldBeInstanceOf<LoadableData.Initial>()
+        initialState.marketChart.shouldBeInstanceOf<LoadableData.Initial>()
+    }
 
-//            val uiState = viewModel.state.value
-//            market shouldBe uiState.market
+    "Given market model, When set market event is triggered, Then market state is updated" {
+        val marketModel = provideMarketList(1).first().toMarketModel()
+
+        viewModel.event(MarketDetailContract.Event.SetMarket(market = marketModel))
+
+        val marketState = viewModel.state.value.market
+        marketState.shouldBeInstanceOf<LoadableData.Loaded<MarketModel>>()
+        marketState.data shouldBe marketModel
+    }
+
+    "Given market detail use case, When get market detail is triggered, Then loading and loaded states are set" {
+        val marketDetail = provideMarketDetail()
+        coEvery { getMarketDetailUseCase(id = any()) } returns flowOf(Resource.Success(marketDetail))
+
+        viewModel.state.test {
+            val initState = awaitItem().marketDetail
+            initState.shouldBeInstanceOf<LoadableData.Initial>()
+
+            viewModel.event(MarketDetailContract.Event.GetMarketDetail(marketId = "testId"))
+
+            val loadingState = awaitItem().marketDetail
+            loadingState.shouldBeInstanceOf<LoadableData.Loading>()
+
+            val loadedState = awaitItem().marketDetail
+            loadedState shouldBe LoadableData.Loaded(data = marketDetail)
         }
     }
-    "With OnFavoriteClick with an item that is already in favorite we remote it from favorite list" {
-        runTest {
-            val market = provideFakeMarket(isFavorite = true)
 
-            val expected = market.copy(isFavorite = false)
+    "Given market chart use case, When get market chart is triggered, Then loading and loaded states are set" {
+        val marketChart = provideMarketChart()
+        coEvery { getMarketChartUseCase(id = any()) } returns flowOf(Resource.Success(marketChart))
 
-            viewModel.event(MarketDetailContract.Event.SetMarket(market))
-            viewModel.event(MarketDetailContract.Event.OnFavoriteClick(market))
-            advanceUntilIdle()
+        viewModel.state.test {
+            val initState = awaitItem().marketChart
+            initState.shouldBeInstanceOf<LoadableData.Initial>()
 
-            coVerify(exactly = 1) {
-                toggleFavoriteMarketListUseCase.invoke(market.toMarket())
-            }
+            viewModel.event(MarketDetailContract.Event.GetMarketChart(marketId = "testId"))
 
-//            val uiState = viewModel.state.value
-//            expected shouldBe uiState.market
+            val loadingState = awaitItem().marketChart
+            loadingState.shouldBeInstanceOf<LoadableData.Loading>()
+
+            val loadedState = awaitItem().marketChart
+            loadedState shouldBe LoadableData.Loaded(data = marketChart)
         }
     }
-    "With OnFavoriteClick with an item that is not in favorite we add it to favorite list" {
-        runTest {
-            val market = provideFakeMarket(isFavorite = false)
 
-            val expected = market.copy(isFavorite = true)
+    "Given market detail use case fails, When get market detail is triggered, Then error state is set" {
+        val exception = RuntimeException("Network Error")
+        coEvery { getMarketDetailUseCase(id = any()) } returns flow { throw exception }
 
-            viewModel.event(MarketDetailContract.Event.SetMarket(market))
-            viewModel.event(MarketDetailContract.Event.OnFavoriteClick(market))
-            advanceUntilIdle()
+        viewModel.event(MarketDetailContract.Event.GetMarketDetail(marketId = "testId"))
 
-            coVerify(exactly = 1) {
-                toggleFavoriteMarketListUseCase(market.toMarket())
-            }
-
-//            val uiState = viewModel.state.value
-//            expected shouldBe uiState.market
-        }
+        val state = viewModel.state.value.marketDetail
+        state.shouldBeInstanceOf<LoadableData.Error>()
+        (state.error as Errors.ExceptionError).message shouldBe exception.message
     }
-    "Get market chart with force refresh is false returns success" {
-        runTest {
-            val market = provideFakeMarket()
-            val chart = provideFakeChart()
-            val chartResult = Resource.Success(chart)
 
-            coEvery { getMarketChartUseCase.invoke(any()) } returns flowOf(chartResult)
+    "Given market chart use case fails, When get market chart is triggered, Then error state is set" {
+        val exception = RuntimeException("Network Error")
+        coEvery { getMarketChartUseCase(id = any()) } returns flow { throw exception }
 
-            viewModel.event(MarketDetailContract.Event.SetMarket(market))
-            viewModel.event(MarketDetailContract.Event.GetMarketChart(market.id))
-            advanceUntilIdle()
+        viewModel.event(MarketDetailContract.Event.GetMarketChart(marketId = "testId"))
 
-//            val uiState = viewModel.state.value
-//            uiState.marketChart shouldBeEqual LoadableData.Loaded(data = chartResult)
-        }
+        val state = viewModel.state.value.marketChart
+        state.shouldBeInstanceOf<LoadableData.Error>()
+        (state.error as Errors.ExceptionError).message shouldBe exception.message
     }
-    "Get market chart with force refresh is false returns error" {
-        runTest {
-            val market = provideFakeMarket()
-            val chartResult = Resource.Error(
-                Errors.ExceptionError(
-                    "Socket Timeout Exception",
-                    throwable = SocketTimeoutException(),
-                ),
-            )
 
-            coEvery { getMarketChartUseCase.invoke(any()) } returns flowOf(chartResult)
+    "Given a market, When favorite click event is triggered, Then toggle favorite use case is called" {
+        val marketModel = provideMarketList(1).first().toMarketModel()
+        coEvery { toggleFavoriteMarketListUseCase(any()) } returns Unit
 
-            viewModel.event(MarketDetailContract.Event.SetMarket(market))
-            viewModel.event(MarketDetailContract.Event.GetMarketChart(market.id))
-            advanceUntilIdle()
+        viewModel.event(MarketDetailContract.Event.SetMarket(market = marketModel))
+        viewModel.event(MarketDetailContract.Event.OnFavoriteClick(market = marketModel))
 
-//            val uiState = viewModel.state.value
-//            uiState.marketChart is LoadableData.Error
-        }
+        coVerify { toggleFavoriteMarketListUseCase(marketModel.toMarket()) }
     }
 })
 
-private fun provideFakeMarket(isFavorite: Boolean = false): MarketModel {
-    return MarketModel(
-        id = UUID.randomUUID().toString(),
-        name = "Bitcoin",
-        symbol = "BTC",
-        imageUrl = "goggle.com",
-        currentPrice = Random.nextDouble(1000.0, 5000.0),
-        priceChangePercentage24h = Random.nextDouble(1000.0, 5000.0),
-        isFavorite = isFavorite,
-    )
-}
+private fun provideMarketList(size: Int, isFavorite: Boolean = false): List<Market> =
+    (0 until size).map {
+        Market(
+            id = UUID.randomUUID().toString(),
+            name = "Market$it",
+            symbol = "SYM",
+            currentPrice = 100.0,
+            priceChangePercentage24h = 1.0,
+            isFavorite = isFavorite,
+            imageUrl = "google.com",
+        )
+    }
 
-private fun provideFakeChart(): MarketChart {
-    return MarketChart(persistentListOf())
-}
+private fun provideMarketDetail(): MarketDetail = MarketDetail(
+    id = UUID.randomUUID().toString(),
+    name = "Test Market",
+    marketCapRank = 1,
+    marketData = null,
+)
+
+private fun provideMarketChart(): MarketChart = MarketChart(
+    prices = persistentListOf(
+        Pair(System.currentTimeMillis(), 100.0),
+        Pair(System.currentTimeMillis(), 101.0),
+    ),
+)
