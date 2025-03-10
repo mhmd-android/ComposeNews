d626a709e998c00f61fe4d28502ab7174432b364
Vahid Garousi <garousiv@gmail.com>
2025-03-10 13:39:08 +0330

    Enable stricter validation for build-logic convention plugins (#282)
    * #280 Refactor: Configure Google Maven repository content for Gradle plugin management and dependency resolution
    
    * #280 Refactor: Configure repository content filter in build-logic
    
    * #280 Add explanation about listing plugins in root project build script
    
    * #280 [refactor] Enable stricter validation
    
    - Enable stricter plugin validation with failOnWarning.
    - set a group for build logic plugin.
    
    * #280 Add `check` task to build-logic in CI workflow

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,6 +1,11 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     `kotlin-dsl`
 }
+
+group = "ir.composenews.buildlogic"
+
 
 // Configure the build-logic plugins to target JDK 17
 // This matches the JDK used to build the project, and is not related to what is running on device.
@@ -8,8 +13,11 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
 kotlin {
-    jvmToolchain(17)
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
 }
 
 dependencies {
@@ -18,6 +26,13 @@ dependencies {
         compileOnly(kotlin.gradlePlugin)
         compileOnly(detekt.gradlePlugin)
         compileOnly(ktlint.kotlinter)
+    }
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
     }
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,13 @@
 
 import org.apache.tools.ant.taskdefs.condition.Os
 
+/*
+ * By listing all the plugins used throughout all subprojects in the root project build script, it
+ * ensures that the build script classpath remains the same for all projects. This avoids potential
+ * problems with mismatching versions of transitive plugin dependencies. A subproject that applies
+ * an unlisted plugin will have that plugin and its dependencies _appended_ to the classpath, not
+ * replacing pre-existing dependencies.
+ */
 plugins {
     libs.plugins.apply {
         alias(android.application) apply false
